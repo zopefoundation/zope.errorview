3ae0de4b9c9a40eb57dda3eef2bfe2a51504be9e
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2011-01-20 11:26:05 +0000

    Preparing release 0.9

---
+++ setup.py
@@ -0,0 +1,71 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+# This package is developed by the Zope Toolkit project, documented here:
+# http://docs.zope.org/zopetoolkit
+# When developing and releasing this package, please follow the documented
+# Zope Toolkit policies as described by this documentation.
+##############################################################################
+"""Setup for zope.errorview package
+"""
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(name='zope.errorview',
+      version='0.9',
+      author='Zope Corporation and Contributors',
+      author_email='zope-dev@zope.org',
+      description='Basic HTTP and Browser exception views.',
+      long_description=(
+          read('README.txt')
+          + '\n\n' +
+          read('CHANGES.txt')
+          ),
+      keywords="zope3 http error view",
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Zope Public License',
+          'Programming Language :: Python',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Topic :: Internet :: WWW/HTTP',
+          'Framework :: Zope3'],
+      url='http://pypi.python.org/pypi/zope.errorview',
+      license='ZPL 2.1',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      namespace_packages=['zope'],
+      extras_require={
+          'test':[
+              'zope.testing'],
+          'browser': [
+              'zope.authentication',
+              'zope.browser',
+              'zope.browserpage',
+          ],
+          },
+      install_requires=[
+          'setuptools',
+          'zope.component',
+          'zope.interface',
+          'zope.publisher',
+          'zope.security',
+          ],
+      include_package_data=True,
+      zip_safe=False,
+      )
